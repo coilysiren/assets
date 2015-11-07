ef6ad42bc1544f2c01beba9e644f27caa58acc7c
Lynn Cyrin <firemagelynn@gmail.com>
2015-11-07 14:00:25 -0800

    update env.py

--- code/env.py
+++ code/env.py
@@ -1,5 +1,21 @@
 # pip install python-dotenv // https://github.com/theskumar/python-dotenv
+#
+# example `.env` file:
+#
+#     USER=lynn
+#     PASS=XXXXXXXXXXXXXX
+#     SECRET_KEY=XXXXXXXXXXXXXXXXXXXXXXXX
 
-from dotenv import parse_dotenv
+from dotenv import parse_dotenv, load_dotenv
 
+# write to os.environ
+#
+# import os
+# os.environ['USER']
+load_dotenv('.env')
+
+# write to ENV object
+#
+# from env import ENV
+# ENV['USER']
 ENV = {item[0]: item[1] for item in parse_dotenv('.env')}

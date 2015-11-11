48646a771a9ca6849a91fc5e30cb24d863ed93d2
Lynn Cyrin <firemagelynn@gmail.com>
2015-11-10 17:14:25 -0800

    update environment handling code

--- code/env.py
+++ code/env.py
@@ -8,16 +8,8 @@
 #     PASS=XXXXXXXXXXXXXX
 #     SECRET_KEY=XXXXXXXXXXXXXXXXXXXXXXXX
 
-from dotenv import parse_dotenv, load_dotenv
+from dotenv import load_dotenv
+from os import environ as ENV
 
-# write to os.environ
-#
-# import os
-# os.environ['USER']
 load_dotenv('.env')
-
-# write to ENV object
-#
-# from env import ENV
-# ENV['USER']
-ENV = {item[0]: item[1] for item in parse_dotenv('.env')}
+ENV['USER'] == 'lynn'

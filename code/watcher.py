1e53987d4372a30cbf47d0ad5d96b05153845ab7
Lynn Cyrin <firemagelynn@gmail.com>
2015-11-16 13:51:07 -0800

    update watcher

--- code/watcher.py
+++ code/watcher.py
@@ -10,13 +10,16 @@ import os
 from glob import glob
 
 
+LOG = logging.getLogger(__name__)
+
+
 class Builder(object):
 
     def __init__(self, assets_dir, output_dir):
         self.assets_dir = assets_dir
         self.output_dir = output_dir
 
-    def sass(self, *args, **kwargs):
+    def compile_sass(self, *args, **kwargs):
         # $ sudo apt-get install python-dev
         # $ pip install libsass
         # https://github.com/dahlia/libsass-python
@@ -32,10 +35,10 @@ class Builder(object):
             filename = sass_file_path.split(self.assets_dir)[-1].split('.')[0]
             css_path = self.output_dir + filename + '.css'
 
-            print('Building {}'.format(css_path))
+            LOG.info('Building \'{}\''.format(css_path))
 
             sass_args = {
-                'source': '{}'.format(sass_path),
+                'source': '{}'.format(sass_file_path),
                 'output': '{}'.format(css_path),
             }
             subprocess.call('''
@@ -45,30 +48,27 @@ class Builder(object):
                 preexec_fn=os.setsid,
                 stdout=subprocess.PIPE
             )
-        print('Completed Build')
+        LOG.info('Completed Build')
 
+    def start(self):
+        from watchdog.observers import Observer
+        from watchdog.events import FileSystemEventHandler
 
-def watcher(callback, path):
-    # create the event handler
-    handler = FileSystemEventHandler()
+        handler = FileSystemEventHandler()
+        handler.on_modified = self.compile_sass
 
-    # define the path and event callback
-    path = os.path.dirname(__file__)+'/'+path
-    handler.on_modified = callback
+        watch = Observer()
+        watch.schedule(handler, self.assets_dir, recursive=True)
+        watch.start()
+        LOG.info('Watching \'{}\' for changes'.format(self.assets_dir))
 
-    # start watching that path
-    watch = Observer()
-    watch.schedule(handler, path, recursive=True)
-    watch.start()
-    print('Watching {} for changes'.format(path))
+def activate():
+    builder = Builder('static/assets/', 'static/css/')
+    builder.start()
+    builder.compile_sass()
 
 
 if __name__ == '__main__':
-
-    assets_dir = 'static/assets/'
-    output_dir = 'static/css/'
-
-    # set configs for the builder class
-    builder = Builder(assets_dir, output_dir)
-    # give the watcher the relevant build function and watch path
-    watcher(builder.sass, assets_dir)
+    builder = Builder('static/assets/', 'static/css/')
+    builder.start()
+    builder.compile_sass()

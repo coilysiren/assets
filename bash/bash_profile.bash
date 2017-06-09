0e4136569606364751a6cf3a11543443a9185811
Lynn Cyrin <firemagelynn@gmail.com>
2017-06-09 11:33:54 -0700

    Create bash_profile.bash

--- bash/bash_profile.bash
+++ bash/bash_profile.bash
@@ -1,5 +1,3 @@
-if which rbenv > /dev/null; then eval "$(rbenv init -)"; fi
-
 parse_git_branch() {
      git branch 2> /dev/null | sed -e '/^[^*]/d' -e 's/* \(.*\)/ (\1)/'
 }
@@ -9,10 +7,6 @@ export LSCOLORS=gxBxhxDxfxhxhxhxhxcxcx
 # user @ computer / filepath (git-granch)
 export PS1="\a\e[31;1m\u@\h\e[33;1m\w\e[0m  \$(parse_git_branch)\[\033[00m\] \n$ "
 
-export ENV="development"
-export RACK_ENV="development"
-export RAILS_ENV="development"
-
 export EDITOR="subl"
 
 grep_open() {
@@ -20,14 +14,16 @@ grep_open() {
 }
 
 alias ls='ls -GFh'
-alias ..='cd ..'
 
 alias node='nodejs'
 
 alias profile-edit='subl ~/.bash_profile'
 alias profile-init='. ~/.bash_profile'
+alias profile-source='profile-edit'
 
 alias st='foreman start'
+
+# Git
 
 alias gt='git status'
 alias gs='git status'
@@ -36,10 +32,18 @@ alias gl='git log --graph --pretty=short'
 alias gush="git push -u origin HEAD"
 alias git-modified="$EDITOR `git status --porcelain | sed -ne 's/^ M //p'`"
 
-alias sv='source .venv/bin/activate'
-alias profile='source ~/.bash_profile'
+# Python
 
+alias sv='source .venv/bin/activate'
 alias pip-remove-all='pip freeze | xargs pip uninstall -y'
 
 if which pyenv > /dev/null; then eval "$(pyenv init -)"; fi
 if which pyenv-virtualenv-init > /dev/null; then eval "$(pyenv virtualenv-init -)"; fi
+
+# Ruby
+
+if which rbenv > /dev/null; then eval "$(rbenv init -)"; fi
+
+export ENV="development"
+export RACK_ENV="development"
+export RAILS_ENV="development"
